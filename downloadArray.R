f433ed03cdb50775233836ec3ce3b8c74485a9fb
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-11-03 10:58:31 -0500

    Create downloadArray.R

---
+++ downloadArray.R
@@ -0,0 +1,19 @@
+require(downloader)
+my.path="/pfs/out"
+
+zipdir1 <- "https://www.ebi.ac.uk/arrayexpress/files/E-MEXP-2458/E-MEXP-2458.raw.1.zip"
+zipdir2 <- "https://www.ebi.ac.uk/arrayexpress/files/E-MEXP-2458/E-MEXP-2458.raw.2.zip"
+
+
+require(R.utils) || stop("Library R.utils is not available!")
+
+dir.create(file.path(my.path, "dwl"), showWarnings=FALSE, recursive=TRUE)
+dir.create(file.path(my.path, "raw"), showWarnings=FALSE, recursive=TRUE)
+
+dwl.status1 <- download(zipdir1, destfile=file.path(my.path, "dwl", "E-MEXP-2458.raw.1.zip"))
+dwl.status2 <- download(zipdir1, destfile=file.path(my.path, "dwl", "E-MEXP-2458.raw.2.zip"))
+                                                          
+unzip(file.path(my.path, "dwl", "E-MEXP-2458.raw.1.zip"), exdir=file.path(my.path, "raw"))
+unzip(file.path(my.path, "dwl", "E-MEXP-2458.raw.2.zip"), exdir=file.path(my.path, "raw"))
+                                                                                     
+unlink(file.path(my.path, "dwl"), recursive=T)
